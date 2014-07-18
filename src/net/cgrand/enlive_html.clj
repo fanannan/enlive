3c9d0c456f63e24801dd20cf5654efd4374ffa85
fanannan <fanannan@users.noreply.github.com>
2014-07-18 20:57:31 +0900

    modified replace-vars (a regex syntax error)

--- src/net/cgrand/enlive_html.clj
+++ src/net/cgrand/enlive_html.clj
@@ -669,7 +669,8 @@
    Does not recurse, you have to pair it with an appropriate selector.
    re is a regex whose first group will be passed to (comp m f) and f by
    default is #'keyword."
-  ([m] (replace-vars #"\$\{\s*([^}]*[^\s}])\s*}" m))
+  ;([m] (replace-vars #"\$\{\s*([^}]*[^\s}])\s*}" m)) ; causes regex syntax error
+  ([m] (replace-vars #"\$\{\s*([^\}]*)\s*\}" m))
   ([re m] (replace-vars re m keyword))
   ([re m f]
     (let [replacement (comp m f second)
